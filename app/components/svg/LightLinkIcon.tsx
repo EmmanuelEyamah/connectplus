46096ba17db513bc737ce3cba841163d7c6c608f
odafe <godfreyj.sule1@gmail.com>
2024-07-02 15:08:56 +0100

    new

---
+++ app/components/svg/LightLinkIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LightLinkIcon = () => {
+  return (
+    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 11.3335L11.6667 4.66683M11.6667 4.66683L5 4.66683M11.6667 4.66683V11.3335" stroke="#201E1E" stroke-opacity="0.9" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    
+  )
+}
+
+export default LightLinkIcon
