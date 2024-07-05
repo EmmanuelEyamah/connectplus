a42fc0677b095d0ac0e17af1e9e1545d60a2ccdf
odafe <godfreyj.sule1@gmail.com>
2024-07-05 11:08:23 +0100

    new files

--- app/About/Hero.tsx
+++ app/About/Hero.tsx
@@ -16,20 +16,19 @@ const Hero = () => {
     setIsPlaying(true);
   };
   return (
-    <section className="flex flex-col items-center pt-20 gap-16 bg-[#F3F3F3] ">
-      <div className=" flex flex-col justify-center items-center gap-5 lg:gap-10 max-w-screen-xl mx-auto">
+    <section className="flex flex-col items-center pt-20 gap-16 bg-[#F3F3F3]  ">
+      <div className=" flex flex-col justify-center items-center gap-5 md:px-15 px-30 lg:gap-10 max-w-screen-xl ">
         <div className=" flex flex-col justify-center items-center gap-2 lg:gap-6 ">
           <BaseTypography
-            label="About Us"
-            fontSizeLarge="lg:text-lg"
-            className=" font-montserrat font-semibold leading-relaxed tracking-normal"
+            label="ABOUT US"
+          
+            className=" font-montserrat lg:text-[18px] text-[14px] fontM  font-semibold leading-relaxed tracking-normal"
             color="text-gray-700"
           />
           <div className="flex flex-col justify-center items-center gap-6">
             <BaseTypography
               label="Our Company"
-              fontSizeLarge="lg:text-[36px]"
-              className=" leading-[48px] font-montserrat font-bold"
+            className=" leading-[48px] lg:text-[48px] text-[35px] work font-bold"
               color="text-black"
             />
             <BaseTypography
@@ -38,12 +37,12 @@ const Hero = () => {
               By cultivating an environment that values both creative ideas and
               technical excellence, we ensure that our solutions not only meet
               but exceed our clients expectations."
-              fontSizeLarge="lg:text-[16px]"
-              className=" leading-[32px] font-medium max-w-screen-lg text-center font-montserrat"
+             
+              className=" leading-[32px] lg:text-[20px] text-[16px] font-medium max-w-screen-lg text-center fontM font-montserrat"
               color="text-black-90"
             />
           </div>
-          <section className="flex items-center gap-6">
+          <section className="flex items-center gap-6 work">
             <AppButton variant="primary">Book A Call</AppButton>
             <AppButton variant="secondary">
               View Project
@@ -53,76 +52,80 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="h-[600px] w-[1248px] rounded-b-[20px] rounded-tr-[120px] relative -bottom-[120px] z-20">
-        {!isPlaying && (
-          <div
-            className="absolute inset-0 flex items-center justify-center cursor-pointer"
-            onClick={handlePlayClick}
-          >
-            <img
-              src="/assets/thumbnail.jpg"
-              alt="Thumbnail"
-              className="w-full h-full object-cover rounded-b-[20px] rounded-tr-[120px]"
-            />
-            <button className="absolute">
-              <svg
-                width="120"
-                height="120"
-                viewBox="0 0 120 120"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="60"
-                  cy="60"
-                  r="59"
-                  stroke="#0A52F6"
-                  strokeWidth="2"
-                />
-                <path d="M96 62L42 89.7128L42 34.2872L96 62Z" fill="#0A52F6" />
-              </svg>
-            </button>
-          </div>
-        )}
-        {isPlaying && (
-          <video
-            className="w-full h-full rounded-b-[20px] rounded-tr-[120px]"
-            src="/assets/team-video.mp4"
-            controls
-            autoPlay
+      <div className="h-[600px] w-full lg:w-[1248px] rounded-b-[20px] rounded-tr-[120px] relative lg:-bottom-[120px] -bottom-[60px] z-20">
+  {!isPlaying && (
+    <div
+      className="absolute inset-0 flex items-center justify-center cursor-pointer"
+      onClick={handlePlayClick}
+    >
+      <img
+        src="/assets/thumbnail.jpg"
+        alt="Thumbnail"
+        className="w-full h-full object-cover rounded-b-[20px] rounded-tr-[120px]"
+      />
+      <button className="absolute">
+        <svg
+          width="80"
+          height="80"
+          viewBox="0 0 120 120"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="60"
+            cy="60"
+            r="59"
+            stroke="#0A52F6"
+            strokeWidth="2"
           />
-        )}
-        <div className="absolute -top-8 -right-5">
-          <UnionIcon />
-        </div>
+          <path d="M96 62L42 89.7128L42 34.2872L96 62Z" fill="#0A52F6" />
+        </svg>
+      </button>
+    </div>
+  )}
+  {isPlaying && (
+    <video
+      className="w-full h-full rounded-b-[20px] rounded-tr-[120px]"
+      src="/assets/team-video.mp4"
+      controls
+      autoPlay
+    />
+  )}
+  <div className="absolute -top-8 -right-5">
+    <UnionIcon />
+  </div>
+</div>
+<div className="bg-[#011F56] container relative w-full h-[500px] lg:h-[400px] lg:pl-0 lg:pt-0 pb-0 flex flex-col lg:flex-row items-center justify-between">
+  <div className="relative flex flex-col lg:flex-col items-center lg:items-start justify-center gap-2  lg:w-2/3 w-full">
+    <div className="flex flex-row lg:flex-col justify-center items-center lg:items-start gap-5 w-full">
+      <div className="relative flex justify-center lg:justify-start">
+        <SmBookIcon />
       </div>
-
-      <div className="bg-[#011F56] container relative w-full h-[500px] lg:h-[450px] lg:px-20 lg:py-36 pl-[220px] flex items-center justify-start lg:justify-end" >
-        <div className="flex flex-col gap-5 w-full">
-          <BaseTypography
-            label="“Success is the result of hard work, unwavering dedication, and relentless effort.”"
-            fontSizeLarge="lg:text-[28px]"
-            className="font-work-sans font-semibold leading-normal w-[250px] lg:w-[519px]"
-            color="text-white"
-          />
-          <BaseTypography
-            label="-Dagogo"
-            fontSizeLarge="lg:text-[24px]"
-            className="font-work-sans font-semibold leading-normal w-[519px]"
-            color="text-white"
-          />
-
-          <div className="absolute left-24 lg:left-60">
-            <BookIcon />
-          </div>
-          <div className="absolute left-0 bottom-0">
-            <PlantIcon />
-          </div>
-          <div className="absolute left-0">
-            <SmBookIcon />
-          </div>
-        </div>
+      <div className="relative flex justify-center lg:justify-start lg:pl-8">
+        <BookIcon />
       </div>
+      <div className="relative flex justify-center lg:justify-start">
+        <PlantIcon />
+      </div>
+    </div>
+  </div>
+  <div className="flex flex-col gap-5 lg:w-1/3 w-full items-center lg:items-start text-center lg:text-left px-5 lg:px-0">
+    <BaseTypography
+      label="“Success is the result of hard work, unwavering dedication, and relentless effort.”"
+      fontSizeLarge=""
+      className="font-work-sans lg:text-[26px] text-[20px] font-semibold leading-normal lg:w-[519px] w-full"
+      color="text-white"
+    />
+    <BaseTypography
+      label="-Dagogo"
+      fontSizeLarge="lg:text-[24px] text-[18px]"
+      className="font-work-sans font-semibold leading-normal lg:w-full w-full"
+      color="text-white"
+    />
+  </div>
+</div>
+
+
     </section>
   );
 };
--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -16,110 +16,111 @@ const SingleBlog = () => {
   const topBlog = blogs.slice(0, 3);
 
   return (
-    <div className="w-full h-full py-20 px-24 bg-white">
-      <div className="w-full h-full container mx-auto flex flex-col items-start gap-16">
-        {/* Blog Title */}
-        <div className="flex flex-col items-start gap-4">
-          <BaseTypography
-            label={blog?.title}
-            fontSizeLarge="lg:text-[48px]"
-            className=" leading-normal font-bold font-work-sans w-[950px]"
-            color="text-[#201E1E]"
-          />
-          <BaseTypography
-            label={blog?.date}
-            fontSizeLarge="lg:text-[20px]"
-            className=" leading-normal font-medium font-montserrat"
-            color="text-[#201E1E]"
-          />
-          <BaseTypography
-            label={blog?.author}
-            fontSizeLarge="lg:text-[20px]"
-            className=" leading-normal font-medium font-montserrat"
-            color="text-[#201E1E]"
-          />
-        </div>
+<div className="w-full h-full py-10 px-5 lg:py-20 lg:px-24 bg-white">
+  <div className="w-full h-full container mx-auto flex flex-col items-start gap-8 lg:gap-16">
+    {/* Blog Title */}
+    <div className="flex flex-col items-start gap-4">
+      <BaseTypography
+        label={blog?.title}
+        fontSizeLarge="text-2xl lg:text-4xl"
+        className="leading-normal font-bold font-work-sans"
+        color="text-[#201E1E]"
+      />
+      <BaseTypography
+        label={blog?.date}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-normal font-medium font-montserrat"
+        color="text-[#201E1E]"
+      />
+      <BaseTypography
+        label={blog?.author}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-normal font-medium font-montserrat"
+        color="text-[#201E1E]"
+      />
+    </div>
 
-        {/* Blog Image */}
-        <div className="w-[1200px] h-[480px]">
-          <img
-            src={blog?.image[1]}
-            alt="case-studies img"
-            className="w-full h-full object-cover"
-          />
-        </div>
+    {/* Blog Image */}
+    <div className="w-full h-64 lg:w-[1200px] lg:h-[480px]">
+      <img
+        src={blog?.image[1]}
+        alt="case-studies img"
+        className="w-full h-full object-cover"
+      />
+    </div>
 
-        {/* Blog Description */}
-        <div className="flex flex-col items-start gap-8">
-          <BaseTypography
-            label={blog?.short_desc}
-            fontSizeLarge="lg:text-[18px]"
-            className=" leading-[29px] font-normal font-montserrat"
-            color="text-[#201E1EE6]"
-          />
-          <BaseTypography
-            label={blog?.description}
-            fontSizeLarge="lg:text-[18px]"
-            className=" leading-[29px] font-normal font-montserrat"
-            color="text-[#201E1EE6]"
-          />
-          <BaseTypography
-            label={blog?.description}
-            fontSizeLarge="lg:text-[18px]"
-            className=" leading-[29px] font-normal font-montserrat"
-            color="text-[#201E1EE6]"
-          />
-          <BaseTypography
-            label={blog?.description}
-            fontSizeLarge="lg:text-[18px]"
-            className=" leading-[29px] font-normal font-montserrat"
-            color="text-[#201E1EE6]"
-          />
-        </div>
+    {/* Blog Description */}
+    <div className="flex flex-col items-start gap-8">
+      <BaseTypography
+        label={blog?.short_desc}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-7 lg:leading-9 font-normal font-montserrat"
+        color="text-[#201E1EE6]"
+      />
+      <BaseTypography
+        label={blog?.description}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-7 lg:leading-9 font-normal font-montserrat"
+        color="text-[#201E1EE6]"
+      />
+      <BaseTypography
+        label={blog?.description}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-7 lg:leading-9 font-normal font-montserrat"
+        color="text-[#201E1EE6]"
+      />
+      <BaseTypography
+        label={blog?.description}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-7 lg:leading-9 font-normal font-montserrat"
+        color="text-[#201E1EE6]"
+      />
+    </div>
 
-        {/* Blog Image */}
-        <div className="w-[1200px] h-[480px]">
-          <img
-            src={blog?.image[1]}
-            alt="case-studies img"
-            className="w-full h-full object-cover"
-          />
-        </div>
+    {/* Blog Image */}
+    <div className="w-full h-64 lg:w-[1200px] lg:h-[480px]">
+      <img
+        src={blog?.image[1]}
+        alt="case-studies img"
+        className="w-full h-full object-cover"
+      />
+    </div>
 
-        {/* Blog Description */}
-        <div className="flex flex-col items-start gap-8">
-          <BaseTypography
-            label={blog?.description}
-            fontSizeLarge="lg:text-[18px]"
-            className=" leading-[29px] font-normal font-montserrat"
-            color="text-[#201E1EE6]"
-          />
-          <BaseTypography
-            label={blog?.description}
-            fontSizeLarge="lg:text-[18px]"
-            className=" leading-[29px] font-normal font-montserrat"
-            color="text-[#201E1EE6]"
-          />
-        </div>
+    {/* Blog Description */}
+    <div className="flex flex-col items-start gap-8">
+      <BaseTypography
+        label={blog?.description}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-7 lg:leading-9 font-normal font-montserrat"
+        color="text-[#201E1EE6]"
+      />
+      <BaseTypography
+        label={blog?.description}
+        fontSizeLarge="text-base lg:text-lg"
+        className="leading-7 lg:leading-9 font-normal font-montserrat"
+        color="text-[#201E1EE6]"
+      />
+    </div>
 
-        {/* Top Blog */}
-        <div className="flex flex-col gap-6">
-          <BaseTypography
-            label="Top Blogs"
-            fontSizeLarge="lg:text-[32px]"
-            className=" leading-normal font-bold font-work-sans"
-            color="text-[#201E1E]"
-          />
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-14 w-full">
-            {topBlog.map((blog) => (
-              <Link key={blog._id} href={`/blog/${blog._id}`}>
-                <BlogCard blog={blog} />
-              </Link>
-            ))}
-          </div>
-        </div>
+    {/* Top Blog */}
+    <div className="flex flex-col gap-6">
+      <BaseTypography
+        label="Top Blogs"
+        fontSizeLarge="text-2xl lg:text-4xl"
+        className="leading-normal font-bold font-work-sans"
+        color="text-[#201E1E]"
+      />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-14 w-full">
+        {topBlog.map((blog) => (
+          <Link key={blog._id} href={`/blog/${blog._id}`}>
+            <BlogCard blog={blog} />
+          </Link>
+        ))}
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
